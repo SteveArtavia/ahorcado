cc284f52e7f5e57441ba94947a2510b7bb5a5466
Steve Artavia <stvartavia@gmail.com>
2025-01-24 12:15:37 -0600

    Logica basica

--- app.js
+++ app.js
@@ -1,10 +1,44 @@
 let palabras = ['manzana', 'puerta', 'tobillo','tocar', 'estupido', 'computadora', 'estadio', 'anillo', 'casa', 'caja', 'gato', 'extra', 'cara', 'bota', 'tornillo','pala','raspar','trabajo', 'doctor', 'empresa', 'abrigo', 'jirafa', 'perro', 'toro', 'ganzo', 'botella', 'libro', 'tuerca', 'carro', 'hogar', 'esposa', 'esposo', 'hijo', 'hija', 'pintar', 'cartera', 'bolso', 'entrada', 'salida', 'cuerno'];
 
-let palabra = generarPalabra();
+let palabraSecreta = generarPalabra();
+let palabraOculta = '';
 
-function generarPalabra(){
-    let palabraRandom = Math.floor(Math.random() * palabras.length);
-    let palabra = palabras[palabraRandom];
-    return palabra;
+function generarPalabra() {
+    let palabra = Math.floor(Math.random() * palabras.length);
+    let palabraRandom = palabras[palabra];
+    palabraRandom = palabraRandom.split('');
+    return palabraRandom;
 }
-console.log(palabra)
+
+function mostrarPalabra() {
+    let palabraHTML = document.getElementById('palabra');
+    palabraOculta = Array(palabraSecreta.length).fill('_');
+    palabraHTML.innerHTML = palabraOculta.join(' ');
+    return;
+}
+
+mostrarPalabra();
+
+function adivinarLetra() {
+    let letra = document.getElementById('letra').value;
+    for(i = 0; i < palabraSecreta.length; i++){
+        if(letra === palabraSecreta[i]){
+            palabraOculta[i] = letra;
+            console.log(palabraOculta[i]);
+        }
+    }
+
+
+    document.getElementById('letra').value = '';
+    actualizarPalabra();
+    return;
+}
+
+function actualizarPalabra(){
+    let palabraHTML = document.getElementById('palabra');
+    palabraHTML.innerHTML = palabraOculta.join(' ');
+    return;
+}
+
+console.log(palabraSecreta);
+console.log(palabraOculta);
